0de6ba655b6e37ebdaab74f3be7f85527e1252d9
Nicolana <nicolanazwj@gmail.com>
2021-01-05 11:35:54 +0800

    save

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+	name = models.CharField(u'姓名', max_length=100, default='匿名')
+	sex = models.CharField('性别', max_length=100, default='匿名')
+	sid = models.CharField('学号', max_length=100, default='0')
+
+	def __str__(self):
+		return '{} {} {}'.format(self.name, self.sex, self.sid)
