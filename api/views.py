988e0db0e0be99f5ad1f48e8d25fdc21711ea2ce
Nicolana <nicolanazwj@gmail.com>
2021-01-06 13:27:16 +0800

    first api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Student
+from .serializers import StudentSerializers
+
+class StudentViewSet(viewsets.ModelViewSet):
+	# 指定结果集并设置排序
+	queryset = Student.objects.all().order_by('-pk')
+	serializer_class = StudentSerializers
